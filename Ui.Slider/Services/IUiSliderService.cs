d6b7d9461af2dae5a74b566cf09fa97967d4d094
anshul@apexol.com <anshul@apexol.com>
2020-01-06 15:46:06 +0530

    moved plugins to public repository

---
+++ Ui.Slider/Services/IUiSliderService.cs
@@ -0,0 +1,10 @@
+﻿using EvenCart.Core.Services;
+using Ui.Slider.Data;
+
+namespace Ui.Slider.Services
+{
+    public interface IUiSliderService : IFoundationEntityService<UiSlider>
+    {
+        
+    }
+}
